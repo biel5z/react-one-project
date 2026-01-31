2ff6a061541fb33955ad915328e13441541ae210
Gabriel Correia Reis <correiareisg@gmail.com>
2026-01-30 21:45:32 -0300

    mol

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import { Circle, Square } from '@/components/Geo'
+
 const Page = () => {
   return (
     <div>
       <h1 className="font-bold text-lg">Fala comigo</h1>
       <h3>Algum</h3>
+
+      <Square></Square>
+      <Circle></Circle>
     </div>
   )
 }
--- components/Square.tsx
+++
@@ -1,9 +0,0 @@
-const Square = () => {
-    return (
-        <div className="w-52 h-52 bg-orange-700 text-white">
-            texto qualquer
-        </div>
-    )
-}
-
-export default Square
