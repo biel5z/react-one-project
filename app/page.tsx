9f071e69ba32024dd976d071575d639156946f25
Gabriel Correia Reis <correiareisg@gmail.com>
2026-02-03 17:00:33 -0300

    Regras gerais do JSX

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import { GeoForm } from "@/components/GeoForm";
+import { Person } from "@/components/Person";
 
 const Page = () => {
   return (
-    <div>
+    <>
       <h1 className="font-bold text-lg">Fala comigo</h1>
       <h3>Algum</h3>
 
       <GeoForm/>
-    </div>
+      <Person/>
+    </>
   )
 }
 
